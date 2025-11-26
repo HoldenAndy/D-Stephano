c16568faf0e9db09dc44a09cacb9f27cdd427e11
HoldenAndy <kaziroz09@gmail.com>
2025-11-26 18:29:09 -0500

    Update new hotjar

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,22 +34,12 @@ export default function RootLayout({
         {children}
         <Toaster />
 
-        {/* --- INICIA EL CÓDIGO DE HOTJAR --- */}
-        {/* 2. AÑADIMOS TU SCRIPT EXACTO AQUÍ */}
-        <Script id="hotjar" strategy="afterInteractive">
-          {`
-            (function (c, s, q, u, a, r, e) {
-                c.hj=c.hj||function(){(c.hj.q=c.hj.q||[]).push(arguments)};
-                c._hjSettings = { hjid: a };
-                r = s.getElementsByTagName('head')[0];
-                e = s.createElement('script');
-                e.async = true;
-                e.src = q + c._hjSettings.hjid + u;
-                r.appendChild(e);
-            })(window, document, 'https://static.hj.contentsquare.net/c/csq-', '.js', 6565494);
-          `}
-        </Script>
-        {/* --- TERMINA EL CÓDIGO DE HOTJAR --- */}
+        <Script 
+          id="contentsquare"
+          src="https://t.contentsquare.net/uxa/4bb9cc536ee51.js" 
+          strategy="afterInteractive" 
+        />
+        {/* --- TERMINA NUEVO SCRIPT DE CONTENTSQUARE --- */}
 
       </body>
     </html>
